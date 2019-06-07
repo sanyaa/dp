65471c1d2260f3a4617c43320b36b6139d4c9c26
sanyaa <mail2lamin@gmail.com>
2019-06-07 14:52:07 +0100

    Create db.php

---
+++ db.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: DP Test
+Plugin URI: https://localhost/
+Description: DP Test.
+Version: 1.0.0
+Author: DP
+Author URI: https://localhost/
+License: GPLv2 or later
+Text Domain: dp
+*/
